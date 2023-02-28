e7d1c0611662a2553b7ae0b00dce29a59014f484
Sofia Enriquez <lsofia.enriquez@gmail.com>
2023-02-28 09:56:39 +0000

    :art: New way of jumping clouds using recursion
    :wq

--- hacker-rank/interview-preparation-kit/warm-up-challenges/jumping_clouds.py
+++ hacker-rank/interview-preparation-kit/warm-up-challenges/jumping_clouds.py
@@ -11,6 +11,17 @@ def jumpingOnClouds(c):
     return jumps
 
 
+def jumpingOnClouds_recursive(c):
+    if len(c) == 1:
+        return 0
+    if len(c) == 2:
+        return 0 if c[1] == 1 else 1
+    if c[2] == 1:
+        return 1 + jumpingOnClouds_recursive(c[1:])
+    if c[2] == 0:
+        return 1 + jumpingOnClouds_recursive(c[1:])
+
+
 if __name__ == "__main__":
 
     case1_clouds = [0, 1, 0, 0, 0, 1, 0]
